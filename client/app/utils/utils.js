531fc95e8a73604d559ecadaf85a558ae8711c4f
Omar <omanpios88@gmail.com>
2024-02-29 07:05:51 -0500

    Create independent functions for /GET and /POST methods and fix login page

--- client/app/utils/utils.js
+++ client/app/utils/utils.js
@@ -1,10 +1,20 @@
-export async function callApi(url = "", method = "GET", data = {}) {
+export async function postData(url = "", method = "POST", data = {}) {
   const response = await fetch(url, {
     method,
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
+  });
+  return response;
+}
+
+export async function getData(url = "", method = "GET") {
+  const response = await fetch(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
   return response;
 }
